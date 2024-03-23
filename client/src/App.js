5e96151f576058f22463907e02f842b12a25d12f
정주리 <mobssie.jung@gmail.com>
2024-03-23 14:04:02 +0900

    fix: client fetch api

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
-
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [ todoList, setTodoList ] = useState([])
+
+  
+  const getData = () => {
+    fetch('http://localhost:4000/api/todo')
+    .then((response) => response.json())
+    .then((data) => setTodoList(data))
+  }
+  
+  const onFetch = (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    const done = e.target.done.checked;
+    fetch('http://localhost:4000/api/todo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text,
+        done
+      })
+    })
+    getData()
+  }
+  
+  useEffect(()=> {
+    getData()
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header"> 
         <h1>TODO LIST</h1>
+        <form onSubmit={onFetch}>
+          <input name="text" />
+          <input name="done" type="checkbox" />
+          <input type="submit" value="추가" />
+        </form>
+        {todoList?.map((todo)=> (
+          <div key={todo.id} style={{ display: 'flex' }}>
+            <div>{todo.id}</div>
+            <div>{todo.text}</div>
+            <div>{todo.done ? 'Y' : 'N'}</div>
+          </div>
+        ))}
       </header>
     </div>
   );
